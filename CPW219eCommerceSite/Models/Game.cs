dca0400d8349cc2bccebb2af41ac09d9065c5888
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-08-30 21:57:25 -0700

    Added game club.

---
+++ CPW219eCommerceSite/Models/Game.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CPW219eCommerceSite.Models
+{
+    /// <summary>
+    /// Represents a single game for purchase
+    /// </summary>
+    public class Game
+    {
+        /// <summary>
+        /// Unique identifier for the game product
+        /// </summary>
+        [Key]
+        public int GameId { get; set; }
+
+        /// <summary>
+        /// The official title of the video game
+        /// </summary>
+        [Required]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The sales price of the video game
+        /// </summary>
+        [Range(0, 1000)]
+        public double Price { get; set; }
+
+        //Todo: Add Rating
+    }
+}
