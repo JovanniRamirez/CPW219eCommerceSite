cae6875d412a8098cda8573dd2096943b55863e6
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-08-30 22:32:06 -0700

    DbContext class

---
+++ CPW219eCommerceSite/Data/VideoGameContext.cs
@@ -0,0 +1,16 @@
+﻿using CPW219eCommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CPW219eCommerceSite.Data
+{
+    public class VideoGameContext : DbContext
+    {
+        public VideoGameContext(DbContextOptions<VideoGameContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Game> Games { get; set; }
+
+
+    }
+}
