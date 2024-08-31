fe08a7dc7802c231790330b1e7b426239e6903b4
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-08-30 22:50:13 -0700

    Configured dbContext

--- CPW219eCommerceSite/Program.cs
+++ CPW219eCommerceSite/Program.cs
@@ -1,6 +1,15 @@
+using CPW219eCommerceSite.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<VideoGameContext>(options => 
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
