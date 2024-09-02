5bcb9f2a734881e67501d2a7a502e4309ac6e7c1
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-09-02 15:29:05 -0700

    Made a create Game view

---
+++ CPW219eCommerceSite/Controllers/GamesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CPW219eCommerceSite.Controllers
+{
+    public class GamesController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
