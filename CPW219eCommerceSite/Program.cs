c5bf0756fddb796e7f52e97b415008c0c421debc
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-09-02 16:24:14 -0700

    added aspnet core again?

--- CPW219eCommerceSite/Program.cs
+++ CPW219eCommerceSite/Program.cs
@@ -1,8 +1,14 @@
-using CPW219eCommerceSite.da
+using CPW219eCommerceSite.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<VideoGameContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDataProtection();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
