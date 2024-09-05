3f129473f23e5a52082d2cdf54d2fee3f77c6c99
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-09-05 14:36:09 -0700

    Add member class

---
+++ CPW219eCommerceSite/Models/Member.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CPW219eCommerceSite.Models
+{
+    public class Member
+    {
+        [Key]
+        public int MemberId { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Phone { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
