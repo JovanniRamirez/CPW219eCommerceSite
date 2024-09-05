4411b947f8df5135a425528f4a0c6636f89ce960
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-09-05 14:56:28 -0700

    Added Register View model

--- CPW219eCommerceSite/Models/Member.cs
+++ CPW219eCommerceSite/Models/Member.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
 
 namespace CPW219eCommerceSite.Models
 {
@@ -14,5 +15,27 @@ namespace CPW219eCommerceSite.Models
         public string Phone { get; set; }
 
         public string UserName { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Compare(nameof(Email))]
+        [StringLength(100)]
+        [Display(Name = "Confirm Email")]
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        [StringLength(75, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
     }
 }
